9ef5118b03bacfa5d70164d3491951db2a45553c
Max <xaphan@bk.ru>
2026-01-11 19:45:32 +0300

    added 15 tables to dataBase - alembic

--- app22/not_async_examples/not_async_db_conf.py
+++ app22/not_async_examples/not_async_db_conf.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from app22.core.config import DATABASE_URL
 
 # """this is so that alembic can see the models and create tables"""
-from app22.async_join_tables.model_admin import *
-from app22.async_join_tables.model_join import *
-from app22.async_join_tables.model_new_ex_db import *
-from app22.async_many_sql.model_new_many_db import *
-from app22.async_reader_project.model_reader_book import *
-from app22.async_reader_project.model_reader_assoc import *
+from app22.async_join_tables import model_new_ex_db
+from app22.async_join_tables import model_join
+from app22.async_join_tables import model_admin
+from app22.async_many_sql import model_new_many_db
+from app22.async_reader_project import model_reader_book
+from app22.async_reader_project import model_reader_assoc
 from app22.db_core.base import Base
 
 
@@ -32,21 +32,23 @@ class SessionDB_not_async:
         """this is so that alembic can see the models and create tables"""
         return (
             Base,
-            User,
-            Post,
-            JoinPerson,
-            JoinAddress,
-            Order,
-            Product,
-            OrderProductAssociation,
-            Admin_list,
-            Admin_work,
-            Reader,
-            ListBook,
-            Book,
-            Category,
-            ListBookAssociation,
-            BookCategoryAssociation,
+            model_new_ex_db.User,
+            model_new_ex_db.Post,
+            model_join.JoinPerson,
+            model_join.JoinAddress,
+            model_admin.Admin_list,
+            model_admin.Admin_work,
+            # many to many
+            model_new_many_db.Order,
+            model_new_many_db.Product,
+            model_new_many_db.OrderProductAssociation,
+            # table association
+            model_reader_book.Reader,
+            model_reader_book.ListBook,
+            model_reader_book.Book,
+            model_reader_book.Category,
+            model_reader_assoc.ListBookAssociation,
+            model_reader_assoc.BookCategoryAssociation,
         )
 
     @staticmethod
